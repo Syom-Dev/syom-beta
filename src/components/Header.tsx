d6d978bdca7f32e28602d0a41bb1620cb304f5c4
AsplemEirik <eirik.asplem@bouvet.no>
2021-07-28 15:48:52 +0200

    Fixed img imports. Added eslint. Ran eslint --fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,79 +1,76 @@
-import React, { FC } from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Box, Container, Typography } from "@material-ui/core"
-import Image from 'next/image'
+import React, { FC } from "react";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import { Box, Container, Typography } from "@material-ui/core";
+import Image from "next/image";
 
 import Scrolldown from "./Scrolldown";
 
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     imageContainer: {
-      height: "100%",
-      width: "100%",
-      margin: "auto",
-      position: "relative",
+        "height": "100%",
+        "width": "100%",
+        "margin": "auto",
+        "position": "relative",
 
-      "&::before": {
-        content: "''",
-        display: "block",
-        background: "url(img/sewdoll.jpg)",
-        backgroundSize: "cover",
-        opacity: 0.5,
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        position: "absolute",
-        zIndex: -1
-      }
+        "&::before": {
+            content: "''",
+            display: "block",
+            background: "url(img/stock/sewdoll.jpg)",
+            backgroundSize: "cover",
+            opacity: 0.5,
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            zIndex: -1
+        }
     },
     fader: {
-      height: "100%",
-      minHeight: theme.contentHeight,
-      width: "100%",
-      background: "linear-gradient(0, white, transparent)",
-      margin: "auto",
-      textAlign: "center",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
+        height: "100%",
+        minHeight: theme.contentHeight,
+        width: "100%",
+        background: "linear-gradient(0, white, transparent)",
+        margin: "auto",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
     }
-  }),
-);
+}));
 
 const SyomHeader: FC = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Box className={classes.imageContainer} >
-      <Box className={classes.fader} py={5}>
-        <Container>
-          <img src="/img/elements/logo1.png" alt="SYOM" style={{ height: "100px" }} />
+    return (
+        <Box className={classes.imageContainer} >
+            <Box className={classes.fader} py={5}>
+                <Container>
+                    <img src="/img/elements/logo1.png" alt="SYOM" style={{ height: "100px" }} />
 
-          <Box py={5}>
-            <Typography component="p" variant="h5">
+                    <Box py={5}>
+                        <Typography component="p" variant="h5">
               Langt liv for klærne
-            </Typography>
-          </Box>
+                        </Typography>
+                    </Box>
 
-          <img src="/img/elements/appview.png" alt="SYOM" style={{ height: "300px" }} />
+                    <img src="/img/elements/appview.png" alt="SYOM" style={{ height: "300px" }} />
 
-          <hr />
-          <Typography component="h1" variant="h2">
+                    <hr />
+                    <Typography component="h1" variant="h2">
             Hvorfor SYOM?
-          </Typography>
+                    </Typography>
 
-          <Typography component="p" >
+                    <Typography component="p" >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Typography>
+                    </Typography>
 
-          <Scrolldown />
-        </Container>
-      </Box>
-    </Box>
-  );
+                    <Scrolldown />
+                </Container>
+            </Box>
+        </Box>
+    );
 };
 
 export default SyomHeader;
--- src/components/TextBoxes/TextImageBox.tsx
+++ src/components/TextBoxes/TextImageBox.tsx
@@ -1,71 +1,64 @@
-import React, { FC } from "react"
-
-import { Box } from "@material-ui/core"
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography } from "@material-ui/core";
-
-import Image from 'next/image'
-
+import React, { FC } from "react";
+import { Box, Typography } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import Image from "next/image";
 
 interface TextImageBoxProps {
-  imageUrl: string;
-  imageAltText: string;
-  heading: string;
-  align?: "left" | "right"
+    imageUrl: string;
+    imageAltText: string;
+    heading: string;
+    align?: "left" | "right";
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
+const useStyles = makeStyles((theme: Theme) => createStyles({
     imageBox: {
-      width: "200px",
-      borderRadius: "50%",
-      position: "relative"
+        width: "200px",
+        borderRadius: "50%",
+        position: "relative"
     },
     rightAligned: {
-      flexDirection: "row-reverse",
-      textAlign: "right",
+        flexDirection: "row-reverse",
+        textAlign: "right"
     },
     leftAligned: {
-      flexDirection: "row",
-      textAlign: "left"
+        flexDirection: "row",
+        textAlign: "left"
     },
     image: {
-      borderRadius: "50%"
+        borderRadius: "50%"
     }
-  }),
-);
-
+}));
 
 export const TextImageBox: FC<TextImageBoxProps> = ({ imageUrl, imageAltText, heading, children, align = "left" }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Box
-      mb={5}
-      display="flex"
-      flexDirection="column"
-      textAlign={align === "left" ? "left" : "right"}
-    >
-      <Typography component="h2" variant="h4">{heading}</Typography>
-      <Box
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        className={align === "right" ? classes.rightAligned : classes.leftAligned}
-      >
-        <Box flexBasis="60%">
-          {children}
-        </Box>
-
+    return (
         <Box
-          className={classes.imageBox}
-          display="flex"
-          alignItems="center"
-          flexBasis="auto"
+            mb={5}
+            display="flex"
+            flexDirection="column"
+            textAlign={align === "left" ? "left" : "right"}
         >
-          <Image src={imageUrl} alt={imageAltText} width={300} height={300} className={classes.image} />
+            <Typography component="h2" variant="h4">{heading}</Typography>
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center"
+                className={align === "right" ? classes.rightAligned : classes.leftAligned}
+            >
+                <Box flexBasis="60%">
+                    {children}
+                </Box>
+
+                <Box
+                    className={classes.imageBox}
+                    display="flex"
+                    alignItems="center"
+                    flexBasis="auto"
+                >
+                    <Image src={imageUrl} alt={imageAltText} width={300} height={300} className={classes.image} />
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  )
-}
+    );
+};

65f57e52724a976d6ac0deabcc978abba7f83ecd
Suhasini Narasimharaj <suhasini.narasimharaj@bouvet.no>
2021-08-04 11:47:12 +0200

    Added footer and required styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,14 @@ import Layout from "../src/components/Layout";
 import SyomHeader from "../src/components/BetaPage/Header";
 import SectionTwo from "../src/components/BetaPage/SectionTwo";
 import SectionThree from "../src/components/BetaPage/SectionThree";
+import { Footer } from "../src/components/Footer";
 
 const Home: FC = () => (
   <Layout>
     <SyomHeader />
     <SectionTwo />
     <SectionThree />
+    <Footer />
   </Layout>
 );
 

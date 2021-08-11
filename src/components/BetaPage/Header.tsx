cf59bf45cf7406f49161ba756a13140cd1c2c60c
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2021-08-11 16:11:01 +0200

    no button in top still works?

--- src/components/BetaPage/Header.tsx
+++ src/components/BetaPage/Header.tsx
@@ -20,7 +20,7 @@ const SyomHeader: FC = () => {
             </Typography>
           </Box>
 
-          <Box mt={4} mb={4}>
+          {/* <Box mt={4} mb={4}>
             <Button
               component={Link}
               className={classes.accceptButton}
@@ -29,7 +29,7 @@ const SyomHeader: FC = () => {
             >
           Ja, jeg vil bidra!
             </Button>
-          </Box>
+          </Box> */}
 
           <Box>
             <img src="/img/elements/appview.svg" alt="SYOM" style={{ maxHeight: "300px" }} />

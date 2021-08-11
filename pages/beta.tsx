6256cd61d82a7d84bdae5f4501f6549a32fd9c8c
Maren Parnas Gulnes <maren.gulnes@bouvet.no>
2021-08-11 16:41:00 +0200

    show content on beta-page

---
+++ pages/beta.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from "react";
+
+import Layout from "../src/components/Layout";
+import SyomHeader from "../src/components/BetaPage/Header";
+import SectionTwo from "../src/components/BetaPage/SectionTwo";
+import SectionThree from "../src/components/BetaPage/SectionThree";
+import { Footer } from "../src/components/Footer";
+
+const Home: FC = () => (
+  <Layout>
+    <SyomHeader />
+    <SectionTwo />
+    <SectionThree />
+    <Footer />
+  </Layout>
+);
+
+export default Home;

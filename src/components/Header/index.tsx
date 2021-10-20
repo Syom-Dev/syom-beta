9757dea790b327d9eaf305e50f1cfd30b1cdfea7
marenpg <maren.gulnes@gmail.com>
2021-10-20 20:07:38 +0200

    salgsvilkår

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+import { Box, Container, Typography, withStyles, WithStyles } from "@material-ui/core";
+
+import { styles } from "./styles.jss";
+
+const SmallSyomHeader: FC<WithStyles<typeof styles>> = ({ children, classes }) => (
+  <Box className={classes.imageContainer} >
+    <Box className={classes.fader} pt={11.5} pb={15}>
+      <Container maxWidth="md">
+        <Box>
+          <img src="/img/elements/logo1.svg" alt="Syom logo" style={{ width: "150px" }} />
+        </Box>
+        {children}
+      </Container>
+    </Box>
+  </Box >
+);
+
+export default withStyles(theme => styles(theme))(SmallSyomHeader);
+
---
+++ src/components/Header/styles.jss.ts
@@ -0,0 +1,22 @@
+import { Theme } from "@material-ui/core/styles";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const styles = (theme: Theme): any => ({
+  imageContainer: {
+    background: "url(img/stock/handsewing6.jpg)",
+    backgroundSize: "cover",
+
+    [theme.breakpoints.down("md")]: {
+      background: "url(img/stock/handsewing5.jpg)",
+      backgroundSize: "cover"
+    }
+  },
+  fader: {
+    height: "100%",
+    width: "100%",
+    background: "linear-gradient(180deg, rgba(255,255,255,0.4) 0%, rgba(255,255,255,0.99) 60%, rgba(255,255,255,1) 100%)",
+    margin: "auto",
+    textAlign: "center"
+  }
+});
+
